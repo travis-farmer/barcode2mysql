e37ca94237a1a823d21a60b045cf78546733195e
Travis Farmer <travis.farmer@outlook.com>
2021-01-23 08:39:55 -0500

    changing method

--- barcode2mysql.ino
+++ barcode2mysql.ino
@@ -1,7 +1,6 @@
+#include <Arduino.h>
 #include <SPI.h>
 #include <WiFi101.h>
-#include <MySQL_Connection.h>
-#include <MySQL_Cursor.h>
 #include "Wire.h"
 #include "Adafruit_LiquidCrystal.h"
 
@@ -10,9 +9,7 @@ Adafruit_LiquidCrystal lcd(0);
 
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-IPAddress server_addr(192,168,1,171);  // IP of the MySQL *server* here
-char user[] = "arduino";              // MySQL user login username
-char password[] = "XXXXXXXX";        // MySQL user login password
+char server[] = "www.tjfhome.net";
 
 // WiFi card example
 char ssid[] = "XXXXXXXX";    // your SSID
@@ -21,7 +18,6 @@ char pass[] = "XXXXXXXX";       // your SSID Password
 
 
 WiFiClient client;
-MySQL_Connection conn((Client *)&client);
 
 // Globals
 String rxBarCode = "";
@@ -34,129 +30,45 @@ String curBarCode = "";
 int curQty = 1;
 bool globalActive = false;
 
-int isNewBC(String inBarCode) {
-  const char QUERY_BC[] = "SELECT Barcode FROM PartsDB.Parts WHERE Barcode = '%s';";
+
+
+void add(String inBarCode, int inQty, String inLoc) {
   char query[128];
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-  
-    // Initiate the query class instance
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    // Supply the parameter for the query
-    // Here we use the QUERY_POP as the format string and query as the
-    // destination. This uses twice the memory so another option would be
-    // to allocate one buffer for all formatted queries or allocate the
-    // memory as needed (just make sure you allocate enough memory and
-    // free it when you're done!).
-    sprintf(query, QUERY_BC, inBarCode);
-    // Execute the query
-    cur_mem->execute(query);
-    
-    // Read the rows and print them
-    row_values *row = NULL;
-    row = cur_mem->get_next_row();
-      if (row != NULL) {
-        return(1); // found the barcode
-      } else {
-        return(2); // did not find the barcode
-      }
-    delete cur_mem;
+  char INSERT_NEW[] = "GET /inventory.php?mode=add&Barcode=%s&Quantity=%d&Location=%s  HTTP/1.1";
+  sprintf(query, INSERT_NEW, inBarCode, inQty, inLoc);
+  if (client.connect(server, 80)) {
+    // Make a HTTP request:
+    client.println(query);
+    client.println("Host: www.tjfhome.net");
+    client.println("Connection: close");
+    client.println();
   }
-  else
-    return(0); // error
 }
 
-int getQty(String inBarCode) {
-  const char QUERY_QTY[] = "SELECT Quantity FROM PartsDB.Parts WHERE Barcode = '%s';";
+void take(String inBarCode, int inQty, String inLoc) {
   char query[128];
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-  
-    // Initiate the query class instance
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    // Supply the parameter for the query
-    // Here we use the QUERY_POP as the format string and query as the
-    // destination. This uses twice the memory so another option would be
-    // to allocate one buffer for all formatted queries or allocate the
-    // memory as needed (just make sure you allocate enough memory and
-    // free it when you're done!).
-    sprintf(query, QUERY_QTY, inBarCode);
-    // Execute the query
-    cur_mem->execute(query);
-    
-    // Read the rows and print them
-    row_values *row = NULL;
-    row = cur_mem->get_next_row();
-    if (row != NULL) {
-      return(row->values[0]);
-    }
-    delete cur_mem;
+  char INSERT_NEW[] = "GET /inventory.php?mode=take&Barcode=%s&Quantity=%d&Location=%s  HTTP/1.1";
+  sprintf(query, INSERT_NEW, inBarCode, inQty, inLoc);
+  if (client.connect(server, 80)) {
+    // Make a HTTP request:
+    client.println(query);
+    client.println("Host: www.tjfhome.net");
+    client.println("Connection: close");
+    client.println();
   }
-  else
-    return(0); // error
-}
-
-void insertNew(String inBarCode, int inQty, String inLoc) {
-  char query[128];
-  char INSERT_NEW[] = "INSERT INTO PartsDB.Parts (Barcode, Quantity, Location) VALUES ('%s',%d ,'%s');";
-
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-    // Initiate the query class instance
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    // Save
-    sprintf(query, INSERT_NEW, inBarCode, inQty, inLoc);
-    // Execute the query
-    cur_mem->execute(query);
-    // Note: since there are no results, we do not need to read any data
-    // Deleting the cursor also frees up memory used
-    delete cur_mem;
-  }
-  conn.close();
-}
-
-bool updateExisting(String inBarCode, int inQty) {
-  char query[128];
-  char UPDATE_EX[] = "UPDATE PartsDB.Parts SET Quantity=%d WHERE Barcode = '%s';";
-
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-    // Initiate the query class instance
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    // Save
-    sprintf(query, UPDATE_EX, inQty, inBarCode);
-    // Execute the query
-    cur_mem->execute(query);
-    // Note: since there are no results, we do not need to read any data
-    // Deleting the cursor also frees up memory used
-    delete cur_mem;
-    return(true); // everything ok
-  }
-  else
-    return(false); // ERROR
-  conn.close();
 }
 
 bool updateMove(String inBarCode, String inLoc) {
   char query[128];
-  char UPDATE_MOV[] = "UPDATE PartsDB.Parts SET Location='%s' WHERE Barcode = '%s';";
-
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-    // Initiate the query class instance
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    // Save
-    sprintf(query, UPDATE_MOV, inLoc, inBarCode);
-    // Execute the query
-    cur_mem->execute(query);
-    // Note: since there are no results, we do not need to read any data
-    // Deleting the cursor also frees up memory used
-    delete cur_mem;
-    return(true); // everything ok
+  char UPDATE_MOV[] = "GET /inventory.php?mode=move&Barcode=%s&Location=%s  HTTP/1.1";
+  sprintf(query, UPDATE_MOV, inBarCode, inLoc);
+  if (client.connect(server, 80)) {
+    // Make a HTTP request:
+    client.println(query);
+    client.println("Host: www.tjfhome.net");
+    client.println("Connection: close");
+    client.println();
   }
-  else
-    return(false); // ERROR
-  conn.close();
 }
 
 void dispLoc() {
@@ -210,23 +122,11 @@ void procTask() {
   // check if ready
   if (curMode > 0 && curLoc != "" && curBarCode != "") {
     if (curMode == 1) { // Add
-      if (isNewBC(curBarCode) == 2 || isNewBC(curBarCode) == 0) { // none stored, or error
-        insertNew(curBarCode,curQty, curLoc); // insert
-      } else { // one, or more stored
-        int storedQtyAdd = getQty(curBarCode);
-        updateExisting(curBarCode,(curQty + storedQtyAdd)); // update DB
-      }
+      add(curBarCode,curQty, curLoc); // insert
     } else if (curMode == 2) { // Take
-      if (isNewBC(curBarCode) == 1) { // must exist to take
-        int storedQtyTake = getQty(curBarCode);
-        if (storedQtyTake >= curQty) { // must have more or equal to take
-          updateExisting(curBarCode,(storedQtyTake - curQty));
-        }
-      }
+      take(curBarCode,curQty,curLoc);
     } else if (curMode == 3) { // Change
-      if (isNewBC(curBarCode) == 1) { // must be there to move
-        updateMove(curBarCode, curLoc);
-      }
+      updateMove(curBarCode, curLoc);
     }
   }
 }
